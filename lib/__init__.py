3206ae098a262e49c94b36fadc983bef4eb8a17f
akscse <aks.stacks@gmail.com>
2021-03-21 12:50:08 +0530

    modularize

---
+++ lib/__init__.py
@@ -0,0 +1,3 @@
+from .perceptron import Perecptron
+from .activation_function import Sigmoid
+from .input_read import Spatial
--- lib/activation_function/Sigmoid.py
+++ lib/activation_function/Sigmoid.py
@@ -1,5 +1,8 @@
+import numpy as np
+
 class Sigmoid:
     def __init__(self):
         return
-    def execute(self):
-        return
+    def execute(self, input):
+        output = 1/(1+np.exp(-input))
+        return output
